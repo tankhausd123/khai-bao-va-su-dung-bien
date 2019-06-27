948dd6a3ea40078c0782cbe268dc6f6dc447021e
ngothanhtan <oh.sick.cf@gmail.com>
2019-06-27 11:13:40 +0700

    ungdungtiente

---
+++ ungdungtiente/doitien.js
@@ -0,0 +1,29 @@
+function myFunction(){
+
+    let amount = document.getElementById('sotien').value;
+    let from = document.getElementById('input').value;
+    let to = document.getElementById('output').value;
+
+    let result;
+
+
+
+    if (from == 'vnd' && to == 'usd2'){
+        result = amount / 23000;
+        document.getElementById('result').innerHTML=result;
+    }
+
+    if (from == 'usd'  && to == 'vnd2'){
+        result = amount*23000;
+        document.getElementById('result').innerHTML=result;
+    }
+    if (from == 'vnd' && to == 'vnd2'){
+        result =amount;
+        document.getElementById('result').innerHTML=result;
+    }
+    if (from == 'usd' && to =='usd2') {
+        result =amount;
+        document.getElementById('result').innerHTML=result;
+    }
+
+}
